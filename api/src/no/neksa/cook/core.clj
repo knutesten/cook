fc3339a66935b8aba11f8bea03f4972fd1371ec7
Knut Esten Melandsø Nekså <knutn@netcompany.com>
2021-08-16 20:51:17 +0200

    Adding nrepl server

--- api/src/no/neksa/cook/core.clj
+++ api/src/no/neksa/cook/core.clj
@@ -1,12 +1,18 @@
 (ns no.neksa.cook.core
   (:require
    [mount.core :refer [defstate start]]
+   [nrepl.server :as nrepl]
    [ring.adapter.jetty :refer [run-jetty]]
    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
    [no.neksa.cook.spec]
    [no.neksa.cook.routes :refer [app]]
    [no.neksa.cook.middleware.edn :refer [wrap-edn-params]])
   (:gen-class))
+
+(defstate nrepl
+  :start (nrepl/start-server :port 4000
+                             :bind "127.0.0.1")
+  :stop (nrepl/stop-server nrepl))
 
 (defstate server
   :start (run-jetty (-> app
